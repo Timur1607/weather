d01d70c161edb554dc053dbffdc918735b7e74c3
Timur <abkeevtimur@gmail.com>
2025-01-01 16:48:21 +0300

    put away mistake

--- script.js
+++ script.js
@@ -6,10 +6,55 @@ let searchInput = document.querySelector('.search__city_input')
 let mistake = document.querySelector('.mistake')
 let tryAgain = document.querySelector('.mistake__tryAgain')
 
-let arr = ['Moscow', "Sochi"]
+// let arr = ['Moscow', "Sochi"]
 let city = ''
+let API = '0f229bddf16ed64444cf47fad115ac05'
+let cod = 0
+let cityTempreture = 0
+let description = ''
+let correctCityName = ''
+
+let temp = document.querySelector('.weather__info_temperature')
 
 let info = document.querySelector('.weather__info_location')
+
+
+
+
+async function getWeather(cityName){
+    const geo = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${cityName}&appid=${API}`)
+    const data = await geo.json()
+    console.log(data);
+    // console.log(data.cod);
+
+    console.log(cod);
+    console.log(data.cod);
+    if(data.cod === 200){
+        console.log('работает');
+        
+        let tempretureInKel = data.main.temp
+        console.log(tempretureInKel);
+        
+        cityTempreture = (tempretureInKel - 273.15).toFixed(1)
+        console.log(cityTempreture);
+        description = data.weather[0].description
+        console.log(description);
+        correctCityName = data.name
+
+        remakeInfoText()
+    } else if(data.cod === '404'){
+        console.log('ошибка');
+        
+        weatherInfo.classList.toggle('info__script')
+        mistake.classList.toggle('mistake__script')
+    }
+    console.log('ты тут');
+    
+
+    cod = data.cod
+    return cityTempreture, description, cod, correctCityName
+}
+
 
 changeCity.addEventListener('click', () => {
     searchScript.classList.toggle('search__script')
@@ -24,37 +69,29 @@ let mistakeWindow = ()  => {
 
 let remakeInfoText = () => {
     let check = false   
-    console.log(searchInput.value.toLowerCase());
-
-    for(let el of arr){
-        console.log(el.toLowerCase());
-        
-        if(el.toLowerCase() === searchInput.value.toLowerCase()){
-            city = el
-            console.log('yes');
-            check = true
-        }
-    }
-
-    if(check === false){
-        mistake.classList.toggle('mistake__script')
-        weatherInfo.classList.toggle('info__script')
-    }
-    info.textContent = `Windy in ${city}`
+    console.log('рили');
+    
+    info.textContent = `${description} in ${correctCityName}`
+    temp.textContent = `${cityTempreture}℃`
 }
+
+let arr = []
 
 let searchingTheWeather = (event) => {           // сам инпут поиска
     event.preventDefault()
+    getWeather(searchInput.value)
+    // console.log(getWeather(searchInput.value));
     
+
     if(searchInput.value === ''){
         mistakeWindow()
         searchInput.value = ''
-    } else{
+    } else if(cod === 404) {
+        mistakeWindow()
+    } else {
         weatherInfo.classList.toggle('info__script')
         searchScript.classList.toggle('search__script')
-        remakeInfoText()
     }
-    
 }
 
 let backToSearch = () => {                  //кнопка назад в поиск города
